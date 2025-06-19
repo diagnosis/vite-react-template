c147d9302d9f55d134e92dea175de104827d8e89
safa demirkan <demirkansafa@gmail.com>
2025-06-19 12:55:12 -0700

    Fix TanStack Router TypeScript syntax error

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 export default defineConfig({
   plugins: [
     TanStackRouterVite({
-      generatedRouteTree: './src/routeTree.gen.js'
+      generatedRouteTree: './src/routeTree.gen.ts'
     }),
     react(),
   ],
